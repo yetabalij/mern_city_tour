c4461fde032fcd6608e2417c532d53276c4c731b
yetabalij <yetabalij@gmail.com>
2022-04-01 16:50:39 +0300

    AddEditTour.js file is added and form is constructed

---
+++ client/src/pages/AddEditTour.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardFooter,
+  MDBValidation,
+  MDBBtn,
+  MDBSpinner,
+} from "mdb-react-ui-kit";
+import ChipInput from "material-ui-chip-input";
+import FileBase from "react-file-base64";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+const initialState = {
+  title: "",
+  description: "",
+  tags: [],
+};
+
+const AddEditTour = () => {
+  const [tourData, setTourData] = useState(initialState);
+  const { title, description, tags } = tourData;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+    setTourData({ ...tourData, [name]: value });
+  };
+  const handleAddTag = (tag) => {
+    setTourData({ ...tourData, tags: [...tourData.tags, tag] });
+  };
+  const handleDeleteTag = (delteTag) => {
+    setTourData({
+      ...tourData,
+      tags: tourData.tags.filter((tag) => tag !== delteTag),
+    });
+  };
+  const handleClear = () => {
+    setTourData({ title: "", description: "", tags: [] });
+  };
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "450px",
+        alignContent: "center",
+        marginTop: "120px",
+      }}
+      className="container"
+    >
+      <MDBCard alignment="center">
+        <h5>Add Tour</h5>
+        <MDBCardBody>
+          <MDBValidation onSubmit={handleSubmit} className="row g-3" noValidate>
+            <div className="col-md-12">
+              <input
+                placeholder="Enter title"
+                type="text"
+                value={title}
+                name="title"
+                onChange={onInputChange}
+                className="form-control"
+                required
+                invalid
+                validation="please provide title"
+              />
+            </div>
+            <div className="col-md-12">
+              <textarea
+                placeholder="Enter description"
+                type="text"
+                style={{ height: "100px" }}
+                value={description}
+                name="description"
+                onChange={onInputChange}
+                className="form-control"
+                required
+                invalid
+                validation="please provide description"
+              />
+            </div>
+            <div className="col-md-12">
+              <ChipInput
+                name="tags"
+                variant="outlined"
+                placeholder="Enter Tag"
+                fullwidth
+                value={tags}
+                onAdd={(tag) => handleAddTag(tag)}
+                onDelete={(tag) => handleDeleteTag(tag)}
+              />
+            </div>
+            <div className="d-flex justify-content-start">
+              <FileBase
+                type="file"
+                multiple={false}
+                onDone={({ base64 }) =>
+                  setTourData({ ...tourData, imageFile: base64 })
+                }
+              />
+            </div>
+            <div className="col-12">
+              <MDBBtn type={{ width: "100%" }}>Submit</MDBBtn>
+              <MDBBtn
+                type={{ width: "100%" }}
+                className="mt-2"
+                color="danger"
+                onClick={handleClear}
+              >
+                Clear
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+        </MDBCardBody>
+      </MDBCard>
+    </div>
+  );
+};
+
+export default AddEditTour;
